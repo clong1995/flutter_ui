0f25059ef3b5c994a2fe356c911b516ecd7d6b2a
huazhimeng <2902151499@qq.com>
2024-01-29 13:50:09 +0800

    兼容桌面

--- rpx/lib/rpx.dart
+++ rpx/lib/rpx.dart
@@ -1,16 +1,20 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class Rpx {
   static double _width = 0;
 
   //当theme中就要配置
-  static void init(BuildContext context,){
-    _width = MediaQuery.of(context).size.width;
+  static void init(
+    BuildContext context,
+  ) {
+    _setWidth(MediaQuery.of(context).size.width);
   }
 
   static Widget builder(BuildContext context, Widget child) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
-    _width = mediaQueryData.size.width;
+    _setWidth(mediaQueryData.size.width);
     return MediaQuery(
       data: mediaQueryData.copyWith(
         textScaleFactor: 1,
@@ -20,9 +24,17 @@ class Rpx {
   }
 
   static double rpx(double size) {
-    if(_width == 0){
+    if (_width == 0) {
       return size;
     }
     return _width / 375 * size;
+  }
+
+  static void _setWidth(double width) {
+    if (Platform.isWindows || Platform.isMacOS) {
+      _width = 375;
+    }else{
+      _width = width;
+    }
   }
 }
