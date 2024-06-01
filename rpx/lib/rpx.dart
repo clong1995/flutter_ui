b804dadfdc9c08192db9a7233623949394e16836
huazhimeng <2902151499@qq.com>
2024-06-01 14:47:13 +0800

    简化逻辑

--- rpx/lib/rpx.dart
+++ rpx/lib/rpx.dart
@@ -3,47 +3,17 @@ import 'package:flutter/material.dart';
 class Rpx {
   static double _width = 0;
 
-  //当theme中就要配置
-  static void init(
-    BuildContext context,
-  ) {
-    _setWidth(MediaQuery.of(context));
-  }
-
-  static Widget builder(BuildContext context, Widget child) {
-    MediaQueryData mediaQueryData = MediaQuery.of(context);
-    _setWidth(mediaQueryData);
-    return MediaQuery(
-      data: mediaQueryData.copyWith(
-        textScaler: const TextScaler.linear(1),
-      ),
-      child: child,
-    );
+  static init(){
+    _width =  WidgetsBinding.instance.platformDispatcher.views.first.physicalSize.width;
   }
 
   static double rpx(double size) {
+    if (size == 0) {
+      return 0;
+    }
     if (_width == 0) {
       return size;
     }
     return _width / 375 * size;
-  }
-
-  static void _setWidth(MediaQueryData mediaQueryData) {
-    /*if (kIsWeb || Platform.isWindows || Platform.isMacOS) {
-      _width = 375;
-    } else {
-      Orientation orientation = mediaQueryData.orientation;
-      if (orientation == Orientation.landscape) {
-        _width = mediaQueryData.size.height;
-      } else {
-        _width = mediaQueryData.size.width;
-      }
-    }*/
-    Orientation orientation = mediaQueryData.orientation;
-    if (orientation == Orientation.landscape) {
-      _width = mediaQueryData.size.height;
-    } else {
-      _width = mediaQueryData.size.width;
-    }
   }
 }
