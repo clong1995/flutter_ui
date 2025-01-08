0ee917e0164de8e23e420b0dbf5be07757da53e2
huazhimeng <2902151499@qq.com>
2025-01-08 10:19:27 +0800

    提交

--- picker/example/lib/main.dart
+++ picker/example/lib/main.dart
@@ -2,7 +2,9 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:picker/def.dart';
-import 'package:picker/file.dart' as file;
+import 'package:picker/file.dart' as picker_file;
+import 'package:picker/image.dart' as picker_image;
+import 'package:picker/video.dart' as picker_video;
 
 void main() {
   runApp(const MyApp());
@@ -33,93 +35,145 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  PickerFile? singleFile;
-  List<PickerFile> multipleFile = [];
-  String? pickerDir;
-  String? saveOut;
+  //image
+  PickerFile? cameraPickerImage;
+  PickerFile? gallerySinglePickerImage;
+  List<PickerFile> galleryMultiplePickerImage = [];
+
+  //video
+  PickerFile? cameraPickerVideo;
+  PickerFile? gallerySinglePickerVideo;
+
+  //file
+  PickerFile? fileSinglePicker;
+  List<PickerFile> fileMultiplePicker = [];
+  String? fileDirPicker;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("图片 和 任意文件选择器"),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text("相册图片操作："),
+            // 通过拍照选取
             FilledButton.icon(
-              icon: Icon(Icons.image_outlined),
-              label: Text("拍照"),
-              onPressed: () {},
+              icon: Icon(Icons.camera_alt_outlined),
+              label: Text("通过拍照选取"),
+              onPressed: () async {
+                cameraPickerImage = await picker_image.camera();
+              },
             ),
+            if (cameraPickerImage != null) Text(cameraPickerImage!.path),
+            // 单选: 选择相册图片
             FilledButton.icon(
               icon: Icon(Icons.image_outlined),
               label: Text("单选: 选择相册图片"),
-              onPressed: () {},
+              onPressed: () async {
+                gallerySinglePickerImage = await picker_image.single();
+              },
             ),
+            if (gallerySinglePickerImage != null)
+              Text(gallerySinglePickerImage!.path),
+            // 多选: 选择相册图片
             FilledButton.icon(
-              icon: Icon(Icons.image_outlined),
+              icon: Icon(Icons.photo_library_outlined),
               label: Text("多选: 选择相册图片"),
-              onPressed: () {},
+              onPressed: () async {
+                galleryMultiplePickerImage = await picker_image.multiple();
+              },
             ),
+            for (int i = 0; i < galleryMultiplePickerImage.length; i++)
+              Text(galleryMultiplePickerImage[i].path),
+            // 保存图片到相册
             FilledButton.icon(
               icon: Icon(Icons.save),
               label: Text("保存图片到相册"),
-              onPressed: () {
-
+              onPressed: () async {
+                await picker_image.save(bytes: Uint8List(0));
               },
             ),
+            SizedBox(height: 20,),
+            Text("相册视频操作："),
+            // 通过录像选取
+            FilledButton.icon(
+              icon: Icon(Icons.video_camera_back_outlined),
+              label: Text("通过录像选取"),
+              onPressed: () async {
+                cameraPickerVideo = await picker_video.camera();
+              },
+            ),
+            if (cameraPickerVideo != null) Text(cameraPickerVideo!.path),
+
+            // 单选: 选择相册视频
+            FilledButton.icon(
+              icon: Icon(Icons.video_file_outlined),
+              label: Text("单选: 选择相册视频"),
+              onPressed: () async {
+                gallerySinglePickerVideo = await picker_video.single();
+              },
+            ),
+            if (gallerySinglePickerVideo != null)
+              Text(gallerySinglePickerVideo!.path),
+            // 保存视频到相册
+            FilledButton.icon(
+              icon: Icon(Icons.save),
+              label: Text("保存视频到相册"),
+              onPressed: () async {
+                await picker_video.save(bytes: Uint8List(0));
+              },
+            ),
+            SizedBox(height: 20,),
+            Text("任意文件操作："),
+            //单选: 选择任意文件
             FilledButton.icon(
               icon: Icon(Icons.description_outlined),
               label: Text("单选: 选择任意文件"),
               onPressed: () {
-                file.single().then((PickerFile? file) {
+                picker_file.single().then((PickerFile? file) {
                   setState(() {
-                    singleFile = file;
+                    fileSinglePicker = file;
                   });
                 });
               },
             ),
-            if (singleFile != null) Text(singleFile!.name),
+            if (fileSinglePicker != null) Text(fileSinglePicker!.path),
+            //多选: 选择任意文件
             FilledButton.icon(
               icon: Icon(Icons.file_copy_outlined),
               label: Text("多选: 选择任意文件"),
               onPressed: () {
-                file.multiple().then((List<PickerFile> list) {
+                picker_file.multiple().then((List<PickerFile> list) {
                   setState(() {
-                    multipleFile = list;
+                    fileMultiplePicker = list;
                   });
                 });
               },
             ),
-            for (int i = 0; i < multipleFile.length; i++)
-              Text(multipleFile[i].name),
+            for (int i = 0; i < fileMultiplePicker.length; i++)
+              Text(fileMultiplePicker[i].path),
+            // 选择目录
             FilledButton.icon(
               icon: Icon(Icons.folder_open),
               label: Text("选择目录"),
               onPressed: () {
-                file.dir().then((String? p) {
+                picker_file.dir().then((String? p) {
                   setState(() {
-                    pickerDir = p;
+                    fileDirPicker = p;
                   });
                 });
               },
             ),
-            if (pickerDir != null) Text(pickerDir!),
+            if (fileDirPicker != null) Text(fileDirPicker!),
+            //保存文件
             FilledButton.icon(
               icon: Icon(Icons.save),
               label: Text("保存文件"),
               onPressed: () {
-                file.save(bytes: Uint8List(0)).then((String? out) {
-                  setState(() {
-                    saveOut = out;
-                  });
-                });
+                picker_file.save(bytes: Uint8List(0));
               },
             ),
-            if (saveOut != null) Text(saveOut!),
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
