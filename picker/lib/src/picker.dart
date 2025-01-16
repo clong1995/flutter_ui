ccaa7af8b2ec18442b53d7ae8fd353cb57bafcea
QiaoJun <1120530717@qq.com>
2025-01-16 16:49:17 +0800

    修复 pickerfile

--- picker/lib/src/picker.dart
+++ picker/lib/src/picker.dart
@@ -1,31 +1,10 @@
-
 import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart';
 
 import '../def.dart';
 
 final ImagePicker imagePicker = ImagePicker();
 
 Future<PickerFile> pickerFile(XFile? xf) async {
-  PickerFile pickerFile = PickerFile()..path = xf?.path ?? "";
-  if (pickerFile.path != "") {
-    if (xf != null && xf.name.isNotEmpty) {
-      pickerFile.name = xf.name;
-    }
-    if (pickerFile.name.isEmpty || pickerFile.name.startsWith(".")) {
-      pickerFile.extension = pickerFile.name;
-      pickerFile.name = "";
-    } else {
-      String ext = extension(pickerFile.path);
-      if (pickerFile.name.length > ext.length) {
-        pickerFile.name =
-            pickerFile.name.substring(0, pickerFile.name.length - ext.length);
-      }
-      pickerFile.extension = ext.replaceAll(RegExp(r'^\.+'), '');
-    }
-
-    final fileSize =  (await  xf?.length()) ?? 0;
-    pickerFile.size = fileSize;
-  }
-  return pickerFile;
+  final fileSize = (await xf?.length()) ?? 0;
+  return PickerXFileImpl(xf, fileSize);
 }
