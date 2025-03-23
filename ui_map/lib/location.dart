a1504524e48f498e3fb29f5b68da867b56725acc
huazhimeng <2902151499@qq.com>
2025-03-23 21:13:52 +0800

    提交

--- ui_map/lib/location.dart
+++ ui_map/lib/location.dart
@@ -1,6 +1,22 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final SharedPreferencesAsync _asyncPrefs = SharedPreferencesAsync();
+const String _key = "__location";
 
 Future<List<double>?> location() async {
+  String? value = await _asyncPrefs.getString(_key);
+  if (value != null && value.isEmpty) {
+    final arr = value.split(",");
+    if (arr.length == 2) {
+      double? lon = double.tryParse(arr[0]);
+      double? lat = double.tryParse(arr[1]);
+      if (lon != null && lat != null) {
+        return [lon, lat];
+      }
+    }
+  }
+
   bool serviceEnabled;
   LocationPermission permission;
   serviceEnabled = await Geolocator.isLocationServiceEnabled();
@@ -32,6 +48,7 @@ Future<List<double>?> location() async {
   // When we reach here, permissions are granted and we can
   // continue accessing the position of the device.
   final position = await Geolocator.getCurrentPosition();
-
-  return [position.longitude,position.latitude];
+  final lonlat = [position.longitude, position.latitude];
+  await _asyncPrefs.setString(_key, lonlat.join(","));
+  return lonlat;
 }
