ae5a34321c5e02eea3cf19fd42bf87e61a5c303a
huazhimeng <2902151499@qq.com>
2025-04-07 14:21:01 +0800

    提交

---
+++ ui_captcha/lib/ui_captcha.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UiCaptcha extends StatelessWidget {
+  const UiCaptcha({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
