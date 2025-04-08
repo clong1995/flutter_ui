1230a9d5e3b7198eeb0611a42e2ff9a417266830
clong <2092151499@qq.com>
2025-04-08 16:25:34 +0800

    提交

--- ui_webview/lib/ui_webview.dart
+++ ui_webview/lib/ui_webview.dart
@@ -6,9 +6,9 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 class UiWebview extends StatefulWidget {
   final String url;
-  final Map<String, Future<Object?> Function(dynamic json)> register;
+  final Map<String, Future<Object?> Function(dynamic json)>? register;
 
-  const UiWebview({super.key, required this.url, required this.register});
+  const UiWebview({super.key, required this.url, this.register});
 
   @override
   State<UiWebview> createState() => _UiWebviewState();
@@ -20,54 +20,56 @@ class _UiWebviewState extends State<UiWebview> {
   @override
   void initState() {
     super.initState();
-
-    final bridge = "_b${random}_";
-    final callbacks = "_cbs${random}_";
-    final callback = "_cb${random}_";
     controller = WebViewController();
-    controller.runJavaScript('''
-      const $callbacks = {};
-      window.$callback = (callbackId, result) =>{
-          if ($callbacks[callbackId]) {
-              $callbacks[callbackId](result);
-              delete $callbacks[callbackId];
-          }
-      }
-      function wvCall(action,param) {
-          return new Promise((resolve) => {
-              const callbackId = 'cb_' + Date.now() + '_' + Math.random().toString(36).substring(2);
-              $callbacks[callbackId] = resolve;
-              const payload = {
-                  callbackId,
-                  action,
-                  param,
-              };
-              $bridge.postMessage(JSON.stringify(payload));
-          });
-      }
-    ''');
-    controller.addJavaScriptChannel(
-      bridge,
-      onMessageReceived: (JavaScriptMessage message) async {
-        final data = jsonDecode(message.message);
-        final callbackId = data['callbackId'];
-        final action = data['action'];
-        final param = data['param'];
-
-        final func = widget.register[action];
-        String res = "";
-        if (func == null) {
-          res = "$action : not found";
+    //有函数需要调用时
+    if (widget.register != null && widget.register!.isNotEmpty) {
+      final bridge = "_b${random}_";
+      final callbacks = "_cbs${random}_";
+      final callback = "_cb${random}_";
+      controller.runJavaScript('''
+        const $callbacks = {};
+        window.$callback = (callbackId, result) =>{
+            if ($callbacks[callbackId]) {
+                $callbacks[callbackId](result);
+                delete $callbacks[callbackId];
+            }
         }
+        function wvCall(action,param) {
+            return new Promise((resolve) => {
+                const callbackId = 'cb_' + Date.now() + '_' + Math.random().toString(36).substring(2);
+                $callbacks[callbackId] = resolve;
+                const payload = {
+                    callbackId,
+                    action,
+                    param,
+                };
+                $bridge.postMessage(JSON.stringify(payload));
+            });
+        }
+      ''');
+      controller.addJavaScriptChannel(
+        bridge,
+        onMessageReceived: (JavaScriptMessage message) async {
+          final data = jsonDecode(message.message);
+          final callbackId = data['callbackId'];
+          final action = data['action'];
+          final param = data['param'];
 
-        final result = await func!(jsonDecode(param));
-        res = jsonEncode(result);
-        final script = '''
+          final func = widget.register![action];
+          String res = "";
+          if (func == null) {
+            res = "$action : not found";
+          }
+
+          final result = await func!(jsonDecode(param));
+          res = jsonEncode(result);
+          final script = '''
                 window.$callback('$callbackId', $res);
               ''';
-        controller.runJavaScript(script);
-      },
-    );
+          controller.runJavaScript(script);
+        },
+      );
+    }
     controller.setJavaScriptMode(JavaScriptMode.unrestricted);
     controller.loadRequest(Uri.parse(widget.url));
   }
@@ -75,5 +77,5 @@ class _UiWebviewState extends State<UiWebview> {
   int get random => Random().nextInt(100);
 
   @override
-  Widget build(BuildContext context) => WebViewWidget(controller: controller,);
+  Widget build(BuildContext context) => WebViewWidget(controller: controller);
 }
