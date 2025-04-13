f4b054aba39e1dd4a9e00aa8bf97dca1064584b3
clong <2092151499@qq.com>
2025-04-13 21:26:11 +0800

    提交

--- ui_page_view/lib/ui_page_view.dart
+++ ui_page_view/lib/ui_page_view.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 class UiPageView extends StatefulWidget {
   final int length;
-  final void Function(int index) listener;
+  final void Function(int index)? listener;
   final Widget Function(TabController controller) child;
 
   const UiPageView({
     super.key,
-    required this.listener,
+    this.listener,
     required this.child,
     required this.length,
   });
@@ -24,7 +24,9 @@ class _UiPageViewState extends State<UiPageView>
   void initState() {
     super.initState();
     _tabController = TabController(length: widget.length, vsync: this);
-    _tabController.addListener(() => widget.listener(_tabController.index));
+    if(widget.listener != null){
+      _tabController.addListener(() => widget.listener!(_tabController.index));
+    }
   }
 
   @override
