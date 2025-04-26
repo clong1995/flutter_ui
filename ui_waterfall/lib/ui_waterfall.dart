7c7b6a48cfcfc39a8f9a2e4078e45e862ce823ca
clong <2092151499@qq.com>
2025-04-26 21:27:09 +0800

    提交

--- ui_waterfall/lib/ui_waterfall.dart
+++ ui_waterfall/lib/ui_waterfall.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:linked_scroll_controller/linked_scroll_controller.dart';
 
 class UiWaterfall<T> extends StatefulWidget {
-  final double? spacing;
-  final double width;
+  final int crossAxisCount;
+  final double mainAxisSpacing;
+  final double crossAxisSpacing;
   final List<UiWaterfallItem<T>> data;
   final Widget Function(T? data) itemBuilder;
 
@@ -11,8 +11,9 @@ class UiWaterfall<T> extends StatefulWidget {
     super.key,
     required this.data,
     required this.itemBuilder,
-    this.spacing,
-    required this.width,
+    this.crossAxisCount = 2,
+    required this.mainAxisSpacing,
+    required this.crossAxisSpacing,
   });
 
   @override
@@ -20,93 +21,29 @@ class UiWaterfall<T> extends StatefulWidget {
 }
 
 class _UiWaterfallState<T> extends State<UiWaterfall<T>> {
-  late double width;
-  late double spacing;
-  late ScrollController leftScrollController;
-  late ScrollController rightScrollController;
-
   @override
   void initState() {
     super.initState();
-    spacing = widget.spacing ?? 5;
-    width = (widget.width - (spacing * 3)) / 2;
-    LinkedScrollControllerGroup linkedScrollControllerGroup =
-        LinkedScrollControllerGroup();
-    leftScrollController = linkedScrollControllerGroup.addAndGet();
-    rightScrollController = linkedScrollControllerGroup.addAndGet();
   }
 
   @override
   Widget build(BuildContext context) {
-    double leftHeight = 0;
-    double rightHeight = 0;
-    List<UiWaterfallItem<T>> leftData = [];
-    List<UiWaterfallItem<T>> rightData = [];
-
-    for (UiWaterfallItem<T> item in widget.data) {
-      double height = item.height;
-      if (rightHeight > leftHeight) {
-        leftData.add(item);
-        leftHeight += height;
-      } else {
-        rightData.add(item);
-        rightHeight += height;
-      }
-    }
-
-    if (rightData.isNotEmpty) {
-      rightHeight += (rightData.length - 1) * spacing;
-    }
-
-    if (leftData.isNotEmpty) {
-      leftHeight += (leftData.length - 1) * spacing;
-    }
-
-    if (rightHeight > leftHeight) {
-      double emptyHeight = rightHeight - leftHeight - spacing;
-      leftData.add(UiWaterfallItem(height: emptyHeight));
-    } else if (rightHeight < leftHeight) {
-      double emptyHeight = leftHeight - rightHeight - spacing;
-      rightData.add(UiWaterfallItem(height: emptyHeight));
-    }
-
-    return Row(
+    return Stack(
       children: [
-        SizedBox(width: spacing),
-        SizedBox(
-          width: width,
-          child: listView(controller: leftScrollController, listData: leftData),
-        ),
-        SizedBox(width: spacing),
-        SizedBox(
-          width: width,
-          child: listView(
-            controller: rightScrollController,
-            listData: rightData,
+        Positioned(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              // constraints 提供父容器的约束信息
+              return Text(
+                '父容器大小: ${constraints.maxWidth} x ${constraints.maxHeight}',
+                style: TextStyle(fontSize: 20),
+              );
+            },
           ),
         ),
-        SizedBox(width: spacing),
       ],
     );
   }
-
-  ListView listView({
-    required ScrollController controller,
-    required List<UiWaterfallItem<T>> listData,
-  }) => ListView.separated(
-    padding: EdgeInsets.zero,
-    controller: controller,
-    itemBuilder: (BuildContext context, int index) {
-      UiWaterfallItem<T> item = listData[index];
-      return SizedBox(
-        height: item.height,
-        child: widget.itemBuilder(item.data),
-      );
-    },
-    separatorBuilder:
-        (BuildContext context, int index) => SizedBox(height: spacing),
-    itemCount: listData.length,
-  );
 }
 
 class UiWaterfallItem<T> {
