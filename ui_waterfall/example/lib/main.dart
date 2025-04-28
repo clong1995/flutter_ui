4c6bb7cbbff673cd6d36a8b786ada3289617b006
huazhimeng <2902151499@qq.com>
2025-04-28 18:02:16 +0800

    提交

--- ui_waterfall/example/lib/main.dart
+++ ui_waterfall/example/lib/main.dart
@@ -22,25 +22,104 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final RegExp regExp = RegExp(r'_([\d]+)x([\d]+)\.$');
+
+  List<Data> data = [
+    Data("A")
+      ..title = "让对方韬光养晦家哦可，怕了不和你家门口受到如此沸腾v个月不婚"
+      ..image =
+          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/1_s_958x1280.png",
+    Data("B")
+      ..title = "发图给v要保护你家门口，了皮"
+      ..image =
+          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/2_s_598x335.png",
+    Data("C")
+      ..title = "下次v不能买"
+      ..image =
+          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/3_s_889x500.png",
+    Data("D")
+      ..title = "让独特风格也不会进口，v个不和你家门口工笔花鸟"
+      ..image =
+          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/4_s_500x750.png",
+    Data("E")
+      ..title = "个与hi就哦可怕，土肥圆轨迹过一会就哦可"
+      ..image =
+          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/5_s_750x500.png",
+    Data("F")
+      ..title = "为儿童与i哦电饭锅后进先出v不能买儿童用户接口的"
+      ..image =
+          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/6_s_960x1280.png",
+    Data("G")
+      ..title = "日发出通过vu有黄牛揭秘看到，墙上的固定风格"
+      ..image =
+          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/7_s_500x332.png",
+    Data("H")
+      ..title = "而饿他副古hi就哦可让的法国红酒，他与hi就一他是度和非i就哦分"
+      ..image =
+          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/8_s_690x1227.png",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: UiWaterfall<String>(
-        data: [
-          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/1_s_958x1280.png",
-          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/2_s_598x335.png",
-          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/3_s_889x500.png",
-          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/4_s_500x750.png",
-          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/5_s_750x500.png",
-          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/6_s_960x1280.png",
-          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/7_s_500x332.png",
-          "https://bj-eschool.oss-cn-beijing.aliyuncs.com/test/8_s_690x1227.png",
-        ],
-        itemBuilder: (data) => Image.network(data, fit: BoxFit.fitWidth),
+      body: UiWaterfall<Data>(
+        data: data,
+        itemBuilder: (data) => itemWidget(data),
       ),
     );
   }
+
+  Widget itemWidget(Data data) {
+    double aspectRatio = 1;
+    Match? match = regExp.firstMatch(data.image);
+    if (match != null) {
+      String? g1 = match.group(1);
+      String? g2 = match.group(2);
+      if (g1 == null || g2 == null) {
+        aspectRatio = 1;
+      } else {
+        double? width = double.tryParse(g1);
+        double? height = double.tryParse(g2);
+        if (width == null || height == null) {
+          aspectRatio = 1;
+        } else {
+          aspectRatio = width / height;
+        }
+      }
+    }
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFFFFF),
+        border: Border.all(color: const Color(0xFF9E9E9E)),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      padding: const EdgeInsets.all(5),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AspectRatio(
+            aspectRatio: aspectRatio,
+            child: Image.network(data.image, fit: BoxFit.cover),
+          ),
+          const SizedBox(height: 5),
+          Text(data.title),
+        ],
+      ),
+    );
+  }
+}
+
+class Data extends UiWaterfallItem {
+  String image = "";
+  String title = "";
+
+  Data(super.key);
 }
--- ui_waterfall/lib/ui_waterfall.dart
+++ ui_waterfall/lib/ui_waterfall.dart
@@ -1,10 +1,15 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-class UiWaterfall<T> extends StatefulWidget {
+class UiWaterfall<T extends UiWaterfallItem> extends StatefulWidget {
   final int crossAxisCount;
   final double mainAxisSpacing;
   final double crossAxisSpacing;
   final List<T> data;
+
+  //如果有网络加载的图片
+  //图片地址最好为 : https://www.abc.com/xxx_s_958x1280.png
+  //其中 958 和 1280 是图片的尺寸，在builder的时候，算出比例使用 AspectRatio 提前设置图片尺寸和位置
+  //达到实现高性能布局，防止因为图片加载而引起的多次全局重绘
   final Widget Function(T data) itemBuilder;
 
   const UiWaterfall({
@@ -12,70 +17,157 @@ class UiWaterfall<T> extends StatefulWidget {
     required this.data,
     required this.itemBuilder,
     this.crossAxisCount = 2,
-    this.mainAxisSpacing = 10,
-    this.crossAxisSpacing = 10,
+    this.mainAxisSpacing = 5,
+    this.crossAxisSpacing = 5,
   });
 
   @override
   State<UiWaterfall<T>> createState() => _UiWaterfallState<T>();
 }
 
-class _UiWaterfallState<T> extends State<UiWaterfall<T>> {
-  late final List<GlobalKey> childKeys;
+class _UiWaterfallState<T extends UiWaterfallItem>
+    extends State<UiWaterfall<T>> {
+  List<_Child> childKeys = [];
+  List<SizedBox> virtualColumn = [];
+  List<_ChildCol<T>> fallColumns = [];
 
   @override
   void initState() {
     super.initState();
-    childKeys = List.generate(widget.data.length, (_) => GlobalKey());
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      for (int i = 0; i < childKeys.length; i++) {
-        final RenderBox? box = childKeys[i].currentContext?.findRenderObject() as RenderBox?;
-        if (box != null) {
-          final Size size = box.size;
-          debugPrint("Item $i size: ${size.width} x ${size.height}");
-        }
-      }
-    });
+    loadData();
+    WidgetsBinding.instance.addPostFrameCallback(layout);
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              double width = constraints.maxWidth;
-              double height = constraints.maxHeight;
-              double virtualHeight =
-                  (width / widget.crossAxisCount) -
-                  ((widget.crossAxisCount - 1) * widget.mainAxisSpacing) / 2;
-              return Container(
-                color: Colors.red,
-                width: virtualHeight,
-                child: ListView(
-                  children:
-                      widget.data.indexed
-                          .map(
-                            (e) => SizedBox(
-                              key: childKeys[e.$1],
-                              child: widget.itemBuilder(e.$2),
-                            ),
-                          )
-                          .toList(),
-                ),
-              );
-            },
-          ),
-        ),
-        Row(
-          children: [
-            Expanded(child: Container(color: Colors.green.withAlpha(100))),
-            SizedBox(width: widget.mainAxisSpacing),
-            Expanded(child: Container(color: Colors.green.withAlpha(100))),
-          ],
-        ),
-      ],
-    );
+  void didUpdateWidget(covariant UiWaterfall<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.crossAxisCount != widget.crossAxisCount ||
+        oldWidget.mainAxisSpacing != widget.mainAxisSpacing ||
+        oldWidget.crossAxisSpacing != widget.crossAxisSpacing ||
+        oldWidget.itemBuilder != widget.itemBuilder ||
+        oldWidget.data != widget.data) {
+      loadData();
+      WidgetsBinding.instance.addPostFrameCallback(layout);
+      setState(() {});
+    }
   }
+
+  void loadData() {
+    //
+    fallColumns = List.generate(widget.crossAxisCount, (_) => _ChildCol<T>());
+    //
+    childKeys = List.generate(
+      widget.data.length,
+      (_) => _Child()..key = GlobalKey(),
+    );
+    virtualColumn =
+        widget.data.indexed
+            .map(
+              (e) => SizedBox(
+                key: childKeys[e.$1].key,
+                child: widget.itemBuilder(e.$2),
+              ),
+            )
+            .toList();
+  }
+
+  void layout(Duration duration) {
+    for (int i = 0; i < childKeys.length; i++) {
+      final _Child child = childKeys[i];
+      final RenderBox? box =
+          child.key.currentContext?.findRenderObject() as RenderBox?;
+      if (box != null) {
+        child.size = box.size;
+      }
+
+      //找到最小的那列
+      final col = colIndex(fallColumns);
+      col.height += child.size.height;
+      col.list.add(widget.data[i]);
+    }
+    //释放布局资源
+    virtualColumn = [];
+    childKeys.clear();
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) => Stack(
+    children: [
+      //计算布局的尺寸
+      Positioned(
+        child: LayoutBuilder(
+          builder:
+              (context, constraints) => SizedBox(
+                width:
+                    (constraints.maxWidth / widget.crossAxisCount) -
+                    ((widget.crossAxisCount - 1) * widget.mainAxisSpacing) / 2,
+                child: SingleChildScrollView(
+                  child: Column(children: virtualColumn),
+                ),
+              ),
+        ),
+      ),
+      CustomScrollView(
+        physics: const AlwaysScrollableScrollPhysics(
+          parent: BouncingScrollPhysics(),
+        ),
+        slivers: [
+          CupertinoSliverRefreshControl(onRefresh: () async {}),
+          // SliverCrossAxisGroup(slivers: slivers)
+          SliverCrossAxisGroup(
+            slivers: List.generate(widget.crossAxisCount * 2 - 1, (col) {
+              if (col.isOdd) {
+                //奇数
+                return SliverConstrainedCrossAxis(
+                  maxExtent: widget.mainAxisSpacing,
+                  sliver: const SliverToBoxAdapter(child: SizedBox.shrink()),
+                );
+              } else {
+                final column = fallColumns[(col / 2).toInt()].list;
+                return SliverList(
+                  // key: ValueKey("${widget.crossAxisCount}-$col"),
+                  delegate: SliverChildBuilderDelegate(
+                    (context, row) => Padding(
+                      key: ValueKey(column[row].key),
+                      padding: EdgeInsets.only(bottom: widget.crossAxisSpacing),
+                      child: widget.itemBuilder(column[row]),
+                    ),
+                    childCount: column.length,
+                  ),
+                );
+              }
+            }),
+          ),
+        ],
+      ),
+    ],
+  );
+
+  _ChildCol<T> colIndex(List<_ChildCol<T>> childCols) {
+    int minIndex = 0;
+    for (int i = 1; i < childCols.length; i++) {
+      if (childCols[i].height < childCols[minIndex].height) {
+        minIndex = i;
+      }
+    }
+    return childCols[minIndex];
+  }
+}
+
+class UiWaterfallItem {
+  dynamic key;
+
+  UiWaterfallItem(this.key);
+}
+
+class _Child {
+  Size size = Size.zero;
+  late GlobalKey key;
+}
+
+class _ChildCol<T> {
+  double height = 0;
+  final List<T> list = [];
 }
