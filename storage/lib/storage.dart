99c2469ad14675a91ae25faf325f48a3033f315c
markeckles <2435965705@qq.com>
2025-05-29 17:21:27 +0800

    提交

--- storage/lib/storage.dart
+++ storage/lib/storage.dart
@@ -3,32 +3,36 @@ import 'package:shared_preferences/shared_preferences.dart';
 class Storage {
   final SharedPreferencesAsync _asyncPrefs = SharedPreferencesAsync();
 
-  Future<void> setInt(String key, int value) => _asyncPrefs.setInt(key, value);
+  Future<void> setInt(String key, int value) async =>
+      await _asyncPrefs.setInt(key, value);
 
-  Future<int?> getInt(String key) async => _asyncPrefs.getInt(key);
+  Future<int?> getInt(String key) async => await _asyncPrefs.getInt(key);
 
   Future<void> setString(String key, String value) async =>
-      _asyncPrefs.setString(key, value);
+      await _asyncPrefs.setString(key, value);
 
-  Future<String?> getString(String key) async => _asyncPrefs.getString(key);
+  Future<String?> getString(String key) async =>
+      await _asyncPrefs.getString(key);
 
   Future<void> setBool(String key, bool value) async =>
-      _asyncPrefs.setBool(key, value);
+      await _asyncPrefs.setBool(key, value);
 
-  Future<bool?> getBool(String key) async => _asyncPrefs.getBool(key);
+  Future<bool?> getBool(String key) async => await _asyncPrefs.getBool(key);
 
   Future<void> setDouble(String key, double value) async =>
-      _asyncPrefs.setDouble(key, value);
+      await _asyncPrefs.setDouble(key, value);
 
-  Future<double?> getDouble(String key) async => _asyncPrefs.getDouble(key);
+  Future<double?> getDouble(String key) async =>
+      await _asyncPrefs.getDouble(key);
 
   Future<void> setStringList(String key, List<String> value) async =>
-      _asyncPrefs.setStringList(key, value);
+      await _asyncPrefs.setStringList(key, value);
 
   Future<List<String>?> getStringList(String key) async =>
-      _asyncPrefs.getStringList(key);
+      await _asyncPrefs.getStringList(key);
 
-  Future<void> remove(String key) async => _asyncPrefs.remove(key);
+  Future<void> remove(String key) async => await _asyncPrefs.remove(key);
 
-  Future<bool> containsKey(String key) async => _asyncPrefs.containsKey(key);
+  Future<bool> containsKey(String key) async =>
+      await _asyncPrefs.containsKey(key);
 }
