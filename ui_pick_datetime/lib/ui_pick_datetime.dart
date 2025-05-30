815a97a77f7f9e632f16b2cf80e5e399dfc60967
markeckles <2435965705@qq.com>
2025-05-30 15:01:46 +0800

    提交

--- ui_pick_datetime/lib/ui_pick_datetime.dart
+++ ui_pick_datetime/lib/ui_pick_datetime.dart
@@ -5,27 +5,27 @@ class UiPickDateTime {
     required BuildContext context,
     required DateTime firstDate,
     required DateTime lastDate,
-    bool useRootNavigator = true,
-    DateTime? initialDate,
+    bool root = true,
+    DateTime? init,
   }) => showDatePicker(
     context: context,
     barrierDismissible: false,
-    useRootNavigator: useRootNavigator,
+    useRootNavigator: root,
     initialEntryMode: DatePickerEntryMode.calendarOnly,
-    initialDate: initialDate,
+    initialDate: init,
     firstDate: firstDate,
     lastDate: lastDate,
   );
 
   static Future<TimeOfDay?> time({
     required BuildContext context,
-    required TimeOfDay initialTime,
-    bool useRootNavigator = true,
+    required TimeOfDay init,
+    bool root = true,
   }) => showTimePicker(
     context: context,
     barrierDismissible: false,
-    useRootNavigator: useRootNavigator,
+    useRootNavigator: root,
     initialEntryMode: TimePickerEntryMode.inputOnly,
-    initialTime: initialTime,
+    initialTime: init,
   );
 }
