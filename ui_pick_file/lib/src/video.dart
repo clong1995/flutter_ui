18b40981068cc1888a5449b09e1d06f85797c1aa
markeckles <2435965705@qq.com>
2025-06-01 16:38:12 +0800

    提交

--- ui_pick_file/lib/src/video.dart
+++ ui_pick_file/lib/src/video.dart
@@ -1,27 +1,26 @@
 import 'dart:typed_data';
 
 import 'package:image_picker/image_picker.dart';
+
+import '../ui_pick_file.dart' show PickerFile;
 import 'picker.dart';
 import 'save_gallery.dart';
 
-
-import '../ui_pick_file.dart' show PickerFile;
-
 class PickVideo {
   //拍摄视频
- static Future<PickerFile?> camera() async {
+  static Future<PickerFile?> camera() async {
     XFile? xf = await imagePicker.pickVideo(source: ImageSource.camera);
     return pickerFile(xf);
   }
 
   //单选 从相册选择视频
- static Future<PickerFile?> gallery() async {
+  static Future<PickerFile?> gallery() async {
     final XFile? xf = await imagePicker.pickVideo(source: ImageSource.gallery);
     return pickerFile(xf);
   }
 
   // 保存视频到相册
- static Future<void> save({String? fileName, required Uint8List bytes}) async {
+  static Future<void> save({String? fileName, required Uint8List bytes}) async {
     await saveVideoToGallery(fileName: fileName, bytes: bytes);
   }
 }
