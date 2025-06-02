3d558f1fa710c160a5690cb388eced81d143a244
markeckles <2435965705@qq.com>
2025-06-02 18:39:50 +0800

    提交

--- nav/lib/nav.dart
+++ nav/lib/nav.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 class Nav {
   static Future<T?> push<T extends Object?>(
-      BuildContext context,
-      Widget Function() page, {
-        bool root = false,
-        Object? args,
-      }) => Navigator.of(context, rootNavigator: root).push<T>(
+    BuildContext context,
+    Widget Function() page, {
+    bool root = false,
+    Object? args,
+  }) => Navigator.of(context, rootNavigator: root).push<T>(
     MaterialPageRoute<T>(
       builder: (BuildContext context) => page(),
       settings: RouteSettings(arguments: args),
@@ -14,23 +14,36 @@ class Nav {
   );
 
   static Future<T?> pushAndRemove<T extends Object?>(
-      BuildContext context,
-      Widget Function() page, {
-        bool root = false,
-        Object? args,
-      }) => Navigator.of(context, rootNavigator: root).pushAndRemoveUntil<T>(
+    BuildContext context,
+    Widget Function() page, {
+    bool root = false,
+    Object? args,
+  }) => Navigator.of(context, rootNavigator: root).pushAndRemoveUntil<T>(
     MaterialPageRoute<T>(
       builder: (BuildContext context) => page(),
       settings: RouteSettings(arguments: args),
     ),
-        (Route<dynamic> route) => false,
+    (Route<dynamic> route) => false,
+  );
+
+  static Future<T?> pushAndReplace<T extends Object?, TO extends Object?>(
+    BuildContext context,
+    Widget Function() page, {
+    bool root = false,
+    Object? args,
+  }) => Navigator.pushReplacement<T, TO>(
+    context,
+    MaterialPageRoute<T>(
+      builder: (BuildContext context) => page(),
+      settings: RouteSettings(arguments: args),
+    ),
   );
 
   static void pop<T extends Object?>(
-      BuildContext context, {
-        bool root = false,
-        T? result,
-      }) => Navigator.of(context, rootNavigator: root).pop<T>(result);
+    BuildContext context, {
+    bool root = false,
+    T? result,
+  }) => Navigator.of(context, rootNavigator: root).pop<T>(result);
 
   static T? routeArgs<T>(BuildContext context) {
     Object? arguments = ModalRoute.of(context)?.settings.arguments;
--- state/lib/logic.dart
+++ state/lib/logic.dart
@@ -5,19 +5,19 @@ import 'package:flutter/scheduler.dart';
 import 'lifecycle.dart';
 import 'src/logic_dict.dart';
 
-abstract class Logic<E> with Lifecycle {
+abstract class Logic<T> with Lifecycle {
   final Map<String, void Function()> _updateDict = {};
 
-  late final E _state;
+  late final T _state;
 
   @nonVirtual
   @protected
-  set state(E value) {
+  set state(T value) {
     _state = value;
   }
 
   @nonVirtual
-  E get state => _state;
+  T get state => _state;
 
   final BuildContext _context;
 
@@ -39,10 +39,6 @@ abstract class Logic<E> with Lifecycle {
   S? find<S>() => LogicDict.get<S>();
 
   @nonVirtual
-  void reload<T>(Widget Function() page) =>
-      pushAndRemove(() => _Reload(page, LogicDict.contain<T>));
-
-  @nonVirtual
   @override
   void update([List<String>? ids]) {
     if (ids != null) {
@@ -55,60 +51,10 @@ abstract class Logic<E> with Lifecycle {
   }
 
   @nonVirtual
-  Widget builder({required String id, required Widget Function(BuildContext context) builder}) =>
-      _BuildChildWidget(id: id, builder: builder, updateDict: _updateDict);
-
-  @nonVirtual
-  void pop<S>([S? result]) => Navigator.pop<S>(_context, result);
-
-  @nonVirtual
-  Future<S?> push<S extends Object?>(
-    Widget Function() page, [
-    Object? arguments,
-  ]) => Navigator.push<S>(
-    _context,
-    MaterialPageRoute<S>(
-      builder: (BuildContext context) => page(),
-      settings: RouteSettings(arguments: arguments),
-    ),
-  );
-
-  @nonVirtual
-  Future<S?> pushAndRemove<S extends Object?>(
-    Widget Function() page, [
-    Object? arguments,
-  ]) => Navigator.pushAndRemoveUntil<S>(
-    _context,
-    MaterialPageRoute<S>(
-      builder: (BuildContext context) => page(),
-      settings: RouteSettings(arguments: arguments),
-    ),
-    (Route<dynamic> route) => false,
-  );
-
-  @nonVirtual
-  Future<S?> pushAndReplace<S extends Object?, SO extends Object?>(
-    Widget Function() page, [
-    Object? arguments,
-  ]) => Navigator.pushReplacement<S, SO>(
-    _context,
-    MaterialPageRoute<S>(
-      builder: (BuildContext context) => page(),
-      settings: RouteSettings(arguments: arguments),
-    ),
-  );
-
-  @nonVirtual
-  S? arguments<S>() {
-    Object? arguments = ModalRoute.of(_context)?.settings.arguments;
-    if (arguments == null) {
-      return null;
-    }
-    if (arguments is S) {
-      return arguments as S;
-    }
-    return null;
-  }
+  Widget builder({
+    required String id,
+    required Widget Function(BuildContext context) builder,
+  }) => _BuildChildWidget(id: id, builder: builder, updateDict: _updateDict);
 
   @nonVirtual
   void Function(FrameCallback callback, {String debugLabel}) frameCallback =
@@ -150,40 +96,4 @@ class _BuildChildWidgetState extends State<_BuildChildWidget> {
     widget.updateDict.remove(widget.id);
     super.dispose();
   }
-}
-
-class _Reload extends StatefulWidget {
-  final Widget Function() page;
-  final bool Function() check;
-
-  const _Reload(this.page, this.check);
-
-  @override
-  State<_Reload> createState() => _ReloadState();
-}
-
-class _ReloadState extends State<_Reload> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback(
-      (Duration timeStamp) => jump(),
-    );
-  }
-
-  void jump() {
-    if (widget.check()) {
-      Future.delayed(const Duration(milliseconds: 500), jump);
-      return;
-    }
-    Navigator.pushAndRemoveUntil(
-      context,
-      MaterialPageRoute(builder: (BuildContext context) => widget.page()),
-      (Route<dynamic> route) => false,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) =>
-      const Center(child: CircularProgressIndicator());
 }
