37a5fdff70ec85357d86bea4c12215518aee916b
markeckles <2435965705@qq.com>
2025-07-28 14:42:29 +0800

    提交

--- dev_simulator/lib/dev_simulator.dart
+++ dev_simulator/lib/dev_simulator.dart
@@ -1,155 +1,141 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+//尺寸: 340 * 740
 class DevSimulator extends StatelessWidget {
-  const DevSimulator({
-    super.key,
-    required this.child,
-  });
+  const DevSimulator(this.builder, {super.key});
 
-  final Widget child;
+  final Widget Function(BuildContext) builder;
+
   final double paddingTop = 35;
-  final double paddingBottom = 26;
+  final double paddingBottom = 25;
+  final double indicatorHeight = 5;
 
   @override
   Widget build(BuildContext context) {
-    if (defaultTargetPlatform == TargetPlatform.windows ||
-        defaultTargetPlatform == TargetPlatform.macOS) {
-      MediaQueryData mediaQueryData = MediaQuery.of(context);
-      return MediaQuery(
-        data: mediaQueryData.copyWith(
-          padding: mediaQueryData.padding
-              .copyWith(top: paddingTop, bottom: paddingBottom),
-        ),
-        child: Scaffold(
-          body: Stack(
-            children: [
-              child,
-              Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                child: Row(
-                  children: [
-                    const Expanded(
-                      child: Center(
-                        child: Text("14:15"),
-                      ),
-                    ),
-                    Container(
-                      width: 180,
-                      height: paddingTop,
-                      decoration: const BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(22),
-                          bottomRight: Radius.circular(22),
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 12,
-                            height: 12,
-                            margin: const EdgeInsets.only(right: 5),
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade900,
-                              borderRadius: BorderRadius.circular(6),
-                            ),
-                          ),
-                          Container(
-                            width: 12,
-                            height: 12,
-                            margin: const EdgeInsets.only(right: 5),
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade900,
-                              borderRadius: BorderRadius.circular(6),
-                            ),
-                          ),
-                          Container(
-                            width: 60,
-                            height: 8,
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade900,
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                          ),
-                          Container(
-                            width: 10,
-                            height: 10,
-                            margin: const EdgeInsets.symmetric(horizontal: 5),
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade900,
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          Container(
-                            width: 15,
-                            height: 15,
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade900,
-                              borderRadius: BorderRadius.circular(7.5),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    const Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Icon(
-                            Icons.signal_cellular_alt,
-                            size: 20,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Icon(
-                            Icons.wifi,
-                            size: 20,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          RotatedBox(
-                            quarterTurns: 1,
-                            child: Icon(
-                              Icons.battery_5_bar,
-                              size: 20,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: MediaQuery(
+              data: mediaQueryData.copyWith(
+                viewPadding: mediaQueryData.padding.copyWith(
+                  top: paddingTop,
+                  bottom: paddingBottom,
                 ),
               ),
-              Positioned(
-                bottom: paddingBottom - 14,
-                // bottom: 10,
-                left: 0,
-                right: 0,
-                child: Center(
-                  child: Container(
-                    height: 5,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(2.5),
+              child: Builder(builder: builder),
+            ),
+          ),
+          //top
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            height: paddingTop,
+            child: Row(
+              children: [
+                const Expanded(child: Center(child: Text("14:15"))),
+                Container(
+                  width: mediaQueryData.size.width / 2,
+                  height: paddingTop,
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(22),
+                      bottomRight: Radius.circular(22),
                     ),
                   ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(right: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade900,
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                      ),
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(right: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade900,
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                      ),
+                      Container(
+                        width: 60,
+                        height: 8,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade900,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                      Container(
+                        width: 10,
+                        height: 10,
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade900,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade900,
+                          borderRadius: BorderRadius.circular(7.5),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Icon(Icons.signal_cellular_alt, size: 20),
+                      SizedBox(width: 5),
+                      Icon(Icons.wifi, size: 20),
+                      SizedBox(width: 5),
+                      RotatedBox(
+                        quarterTurns: 1,
+                        child: Icon(Icons.battery_5_bar, size: 20),
+                      ),
+                      SizedBox(width: 10),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          //bottom
+          Positioned(
+            height: paddingBottom,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Container(
+                height: indicatorHeight,
+                width: mediaQueryData.size.width / 2,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(indicatorHeight / 2),
                 ),
               ),
-            ],
+            ),
           ),
-        ),
-      );
-    }
-    return child;
+        ],
+      ),
+    );
   }
 }
+
+// 34
+// 896 740
