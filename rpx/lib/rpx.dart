b80b3d240c864714335723244e8e1ccdc269f40d
markeckles <2435965705@qq.com>
2025-07-28 10:29:56 +0800

    提交

--- rpx/lib/rpx.dart
+++ rpx/lib/rpx.dart
@@ -1,23 +1,32 @@
-import 'dart:io';
 import 'dart:math';
 import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
 
 class Rpx {
   static double _width = 0;
 
   static init([double? width]) {
-    if (Platform.isMacOS || Platform.isLinux || Platform.isWindows) {
+    if (width != null) {
+      _width = width;
       return;
     }
 
-    if (width != null) {
-      _width = width;
-    } else {
-      FlutterView flutterView = PlatformDispatcher.instance.views.first;
-      double pw = flutterView.physicalSize.width;
-      double ph = flutterView.physicalSize.height;
-      _width = min(pw, ph) / flutterView.devicePixelRatio;
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.macOS:
+      case TargetPlatform.linux:
+      case TargetPlatform.windows:
+        //固定窗口大小建议传入width
+        //非固定窗口，建议传入初始化窗口的width，并设置最小窗口尺寸。
+        //网页也能检测到走到这个分支，建议传入width，设置网页最小尺寸
+        return;
+      default:
+        break;
     }
+    FlutterView flutterView = PlatformDispatcher.instance.views.first;
+    double pw = flutterView.physicalSize.width;
+    double ph = flutterView.physicalSize.height;
+    _width = min(pw, ph) / flutterView.devicePixelRatio;
   }
 
   static double rpx(double size) {
