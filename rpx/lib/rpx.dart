2798b1e31de7dca2974724c76367c5c1c74000ab
markeckles <2435965705@qq.com>
2025-07-28 10:32:50 +0800

    提交

--- rpx/lib/rpx.dart
+++ rpx/lib/rpx.dart
@@ -19,6 +19,9 @@ class Rpx {
         //固定窗口大小建议传入width
         //非固定窗口，建议传入初始化窗口的width，并设置最小窗口尺寸。
         //网页也能检测到走到这个分支，建议传入width，设置网页最小尺寸
+        if(kDebugMode){//网页开发可能在桌面端，继续走适配逻辑
+          break;
+        }
         return;
       default:
         break;
