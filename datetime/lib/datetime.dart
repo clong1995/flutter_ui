575d2a468d38e127f9d3aad71831971bd40349a0
markeckles <2435965705@qq.com>
2025-08-02 18:16:10 +0800

    提交

--- datetime/lib/datetime.dart
+++ datetime/lib/datetime.dart
@@ -67,6 +67,15 @@ class Datetime {
     return [];
   }
 
+  //当前季度
+  static int quarter(DateTime date) {
+    final month = date.month;
+    if (month <= 3) return 1;
+    if (month <= 6) return 2;
+    if (month <= 9) return 3;
+    return 4;
+  }
+
   static String weekDayStr(String date) {
     final datetime = DateTime.parse(date);
     if (datetime.weekday == DateTime.monday) {
