6e5431b90ef18cbb66eb5f990d0645431f59312c
markeckles <2435965705@qq.com>
2026-02-09 00:41:40 +0800

    提交

--- ui_toast/lib/ui_toast.dart
+++ ui_toast/lib/ui_toast.dart
@@ -1,71 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ui_button/ui_button.dart';
-
-/*import 'package:ui_toast/src/toast_widget.dart';
-
-class UiToast {
-  static UiToastMessage get success => UiToastMessage()
-    ..icon = const Icon(Icons.check_circle_outline, color: Colors.green)
-    ..text = '成功'
-    ..color = Colors.green;
-
-  static UiToastMessage get info => UiToastMessage()
-    ..icon = const Icon(Icons.info_outline, color: Colors.orange)
-    ..text = '提示'
-    ..color = Colors.orange;
-
-  static UiToastMessage get failure => UiToastMessage()
-    ..icon = const Icon(Icons.cancel_outlined, color: Colors.red)
-    ..text = '失败'
-    ..color = Colors.red;
-
-  static UiToastMessage get loading => UiToastMessage()
-    ..icon = const SizedBox(
-      width: 20,
-      height: 20,
-      child: CircularProgressIndicator(color: Colors.blue),
-    )
-    ..text = '加载中'
-    ..color = Colors.blue
-    ..autoClose = false;
-
-  static UiToastMessage get choice => UiToastMessage()
-    ..icon = const Icon(Icons.help_outline, color: Colors.blue)
-    ..text = '选择'
-    ..color = Colors.blue
-    ..autoClose = false
-    ..choiceCallback = (bool choice) {};
-
-  static void show(UiToastMessage message) {
-    if (update == null) {
-      return;
-    }
-    message.text = message.text.trim();
-    update!.call(message);
-    if (message.autoClose) {
-      Future.delayed(const Duration(seconds: 1), () => update!.call(null));
-    }
-  }
-
-  static void dismiss() => update?.call(null);
-}
-
-Widget uiToastBuilder(BuildContext context, Widget? child) => Stack(
-  children: [
-    child ?? const SizedBox.shrink(),
-    const Positioned.fill(child: ToastWidget()),
-  ],
-);
-
-class UiToastMessage {
-  Widget icon = const Icon(Icons.circle_outlined);
-  String text = '无';
-  Color color = Colors.grey;
-  bool autoClose = true;
-  void Function(bool choice)? choiceCallback;
-}*/
 
 class UiToast {
   UiToast._();
@@ -115,37 +52,42 @@ class UiToastMessage {
   UiToastMessage();
 
   factory UiToastMessage.success() => UiToastMessage()
-    ..icon = const Icon(
-      Icons.check_circle_outline,
-      color: const Color(0xFF4CAF50),
+    ..icon = const FaIcon(
+      FontAwesomeIcons.circleCheck,
+      color: Color(0xFF4CAF50),
     )
     ..text = '成功'
     ..color = const Color(0xFF4CAF50);
 
   factory UiToastMessage.info() => UiToastMessage()
-    ..icon = const Icon(Icons.info_outline, color: Color(0xFFFF9800))
+    ..icon = const FaIcon(FontAwesomeIcons.circleDot, color: Color(0xFFFF9800))
     ..text = '提示'
     ..color = const Color(0xFFFF9800);
 
   factory UiToastMessage.failure() => UiToastMessage()
-    ..icon = const Icon(Icons.cancel_outlined, color: Colors.red)
+    ..icon = const FaIcon(
+      FontAwesomeIcons.circleXmark,
+      color: Color(0xFFF44336),
+    )
     ..text = '失败'
-    ..color = Colors.red;
+    ..color = const Color(0xFFF44336);
 
   factory UiToastMessage.loading() => UiToastMessage()
-    ..icon = SizedBox(
-      width: 20,
-      height: 20,
-      child: CircularProgressIndicator(color: const Color(0xFF2196F3)),
+    ..icon = const FaIcon(
+      FontAwesomeIcons.spinner,
+      color: Color(0xFF2196F3),
     )
     ..text = '加载中'
     ..color = const Color(0xFF2196F3)
     ..autoClose = false;
 
-  Widget icon = const Icon(Icons.circle_outlined);
+  Widget icon = const FaIcon(FontAwesomeIcons.circle);
   String text = '无';
   Color color = const Color(0xFF000000);
   bool autoClose = true;
+
+  //
+  // ignore:avoid_positional_boolean_parameters
   void Function(bool choice)? choiceCallback;
 }
 
