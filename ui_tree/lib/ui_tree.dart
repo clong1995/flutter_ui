7d3eec8f7dfd8567582e32c76185f59c574d3330
markeckles <2435965705@qq.com>
2026-05-04 14:44:13 +0800

    提交

--- ui_tree/lib/ui_tree.dart
+++ ui_tree/lib/ui_tree.dart
@@ -1,40 +1,41 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' show Icons;
+import 'package:flutter/widgets.dart';
 import 'package:rpx/ext.dart';
+import 'package:ui_theme/ui_theme.dart';
 
 class UiTree<T extends Comparable<T>> extends StatefulWidget {
   const UiTree({
     required this.data,
-    required this.itemBuilder,
+    this.itemBuilder,
     super.key,
     this.indent,
     this.selectedId = '',
+    this.expandedId,
     this.onTap,
   });
 
   final double? indent;
   final String selectedId;
+  final List<String>? expandedId;
   final List<UiTreeItem<T>> data;
   final Widget Function(
     BuildContext context,
-    UiTreeItem<T> item,
-    int length,
-    int level,
-    bool expand,
-    bool selected,
-  )
+    UiTreeItemOne<T> item,
+  )?
   itemBuilder;
 
-  final void Function(UiTreeItem<T> item)? onTap;
+  final void Function(String selectedId,List<String> expandedId)? onTap;
 
   @override
   State<UiTree<T>> createState() => _UiTreeState<T>();
 }
 
 class _UiTreeState<T extends Comparable<T>> extends State<UiTree<T>> {
-  List<_Tree<T>> treeList = [];
+  List<UiTreeItemBranch<T>> treeList = [];
 
   String selectedId = '';
+  List<String> expandedId = [];
 
   late Color activeBackgroundColor;
 
@@ -64,48 +65,94 @@ class _UiTreeState<T extends Comparable<T>> extends State<UiTree<T>> {
     );
   }
 
-  Widget buildItem(_Tree<T> tree) {
-    tree.selected = selectedId == tree.item.id;
+  Widget buildItem(UiTreeItemBranch<T> treeBranch) {
+    treeBranch.item.selected = selectedId == treeBranch.item.item.id;
+
     return Padding(
-      key: ValueKey(tree.item.id),
-      padding: EdgeInsets.only(left: widget.indent ?? 10.r * tree.level),
+      key: ValueKey(treeBranch.item.item.id),
+      padding: EdgeInsets.only(
+        left: widget.indent ?? 10.r * treeBranch.item.level,
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           GestureDetector(
             onTap: () {
-              if (selectedId != tree.item.id) {
-                widget.onTap?.call(tree.item);
+              if (selectedId != treeBranch.item.item.id) {
+                widget.onTap?.call(selectedId,expandedId);
               }
-              selectedId = tree.item.id;
-              if (tree.expand) {
+              selectedId = treeBranch.item.item.id;
+              if (treeBranch.item.expand) {
                 //展开
-                if (tree.selected) {
+                if (treeBranch.item.selected) {
                   //被选中
-                  tree.expand = false; //收起
+                  treeBranch.item.expand = false; //收起
+                  expandedId.removeWhere((e) => e == treeBranch.item.item.id);
                 } else {
                   //没被选中
                   //啥也不干
                 }
               } else {
                 //关闭
-                tree.expand = true;
+                treeBranch.item.expand = true;
+                expandedId.add(treeBranch.item.item.id);
               }
               setState(() {});
             },
-            child: widget.itemBuilder(
-              context,
-              tree.item,
-              tree.children.length,
-              tree.level,
-              tree.expand,
-              tree.selected,
-            ),
+            child: widget.itemBuilder == null
+                ? itemBuilder(context, treeBranch.item)
+                : widget.itemBuilder!(context, treeBranch.item),
           ),
-          Visibility(
-            visible: tree.expand,
-            child: Column(
-              children: tree.children.map(buildItem).toList(),
+          if (treeBranch.children.isNotEmpty)
+            Visibility(
+              visible: treeBranch.item.expand,
+              child: Column(
+                children: treeBranch.children.map(buildItem).toList(),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget itemBuilder(
+    BuildContext context,
+    UiTreeItemOne<T> item,
+  ) {
+    final selectedFontColor = item.selected ? UiTheme.white : UiTheme.black;
+    final selectedFontWeight = item.selected ? FontWeight.bold : null;
+    final selectedBackgroundColor = item.selected
+        ? UiTheme.primaryColor
+        : UiTheme.white;
+    return Container(
+      key: ValueKey(item.item.id),
+      height: 34.r,
+      decoration: BoxDecoration(
+        color: selectedBackgroundColor,
+        borderRadius: BorderRadius.circular(5.r),
+      ),
+      margin: EdgeInsets.only(bottom: 4.r),
+      padding: EdgeInsets.symmetric(horizontal: 5.r),
+      child: Row(
+        children: [
+          if (item.len == 0)
+            SizedBox(
+              width: 5.r,
+            )
+          else
+            Icon(
+              item.expand ? Icons.arrow_drop_down : Icons.arrow_right,
+              color: selectedFontColor,
+            ),
+          Expanded(
+            child: Text(
+              item.item.title,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                color: selectedFontColor,
+                fontWeight: selectedFontWeight,
+              ),
             ),
           ),
         ],
@@ -115,11 +162,18 @@ class _UiTreeState<T extends Comparable<T>> extends State<UiTree<T>> {
 
   void buildTree() {
     selectedId = widget.selectedId;
+    expandedId = widget.expandedId ?? <String>[];
+    if (selectedId.isNotEmpty) {
+      expandedId.add(selectedId);
+    }
+    expandedId = expandedId.toSet().toList();
+
     treeList.clear();
-    final nodeMap = <String, _Tree<T>>{};
+    final nodeMap = <String, UiTreeItemBranch<T>>{};
     for (final item in widget.data) {
-      nodeMap[item.id] = _Tree<T>()
-        ..item = item
+      nodeMap[item.id] = UiTreeItemBranch<T>()
+        ..item = UiTreeItemOne<T>()
+        ..item = UiTreeItemOne<T>()
         ..children = [];
     }
 
@@ -127,15 +181,16 @@ class _UiTreeState<T extends Comparable<T>> extends State<UiTree<T>> {
       final node = nodeMap[item.id]!;
 
       if (item.pid.isEmpty) {
-        node.level = 0;
+        node.item.level = 0;
         treeList.add(node);
       } else {
         final parentNode = nodeMap[item.pid];
         if (parentNode != null) {
-          node.level = parentNode.level + 1;
+          node.item.level = parentNode.item.level + 1;
           parentNode.children.add(node);
+          parentNode.item.len = parentNode.children.length;
         } else {
-          node.level = 0;
+          node.item.level = 0;
           treeList.add(node);
         }
       }
@@ -161,10 +216,16 @@ class _UiTreeState<T extends Comparable<T>> extends State<UiTree<T>> {
 
 @immutable
 class UiTreeItem<T extends Comparable<T>> {
-  const UiTreeItem({required this.id, required this.data, this.pid = ''});
+  const UiTreeItem({
+    required this.id,
+    required this.data,
+    required this.title,
+    this.pid = '',
+  });
 
   final String id;
   final String pid;
+  final String title;
   final T data;
 
   @override
@@ -180,10 +241,15 @@ class UiTreeItem<T extends Comparable<T>> {
   int get hashCode => Object.hash(id, pid, data);
 }
 
-class _Tree<T extends Comparable<T>> {
+class UiTreeItemBranch<T extends Comparable<T>> {
+  late UiTreeItemOne<T> item;
+  List<UiTreeItemBranch<T>> children = [];
+}
+
+class UiTreeItemOne<T extends Comparable<T>> {
   int level = 0;
   bool expand = false;
   bool selected = false;
+  int len = 0;
   late UiTreeItem<T> item;
-  List<_Tree<T>> children = [];
 }
