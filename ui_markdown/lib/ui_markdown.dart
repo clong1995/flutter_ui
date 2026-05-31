dea3e4cdf125fe0c7440db4ff6c2c90a639a2b8b
markeckles <2435965705@qq.com>
2026-06-01 00:14:29 +0800

    提交

--- ui_markdown/lib/ui_markdown.dart
+++ ui_markdown/lib/ui_markdown.dart
@@ -2,10 +2,16 @@ import 'package:flutter/widgets.dart';
 import 'package:gpt_markdown/gpt_markdown.dart';
 
 class UiMarkdown extends StatelessWidget {
-
   const UiMarkdown(this.data, {super.key});
+
   final String data;
 
   @override
-  Widget build(BuildContext context) => GptMarkdown(data);
+  Widget build(BuildContext context) => GptMarkdown(
+    data,
+    imageBuilder: (context, uri, title, alt) => Image.network(
+      uri,
+      fit: BoxFit.fitWidth,
+    ),
+  );
 }
