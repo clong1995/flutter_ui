198f2d22673da01f52b776156ff3e2977d659c72
markeckles <2435965705@qq.com>
2026-07-09 12:14:43 +0800

    提交

--- ui_disable/lib/ui_disable.dart
+++ ui_disable/lib/ui_disable.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/widgets.dart';
 
 class UiDisable extends StatelessWidget {
-  const UiDisable({required this.child, super.key});
+  const UiDisable({this.disable = true, required this.child, super.key});
 
   final Widget child;
+  final bool disable;
 
   @override
-  Widget build(BuildContext context) => AbsorbPointer(
-    child: ColorFiltered(
-      colorFilter: const .matrix(<double>[
-        0.2126, 0.7152, 0.0722, 0, 0, // 红色通道的新值
-        0.2126, 0.7152, 0.0722, 0, 0, // 绿色通道的新值
-        0.2126, 0.7152, 0.0722, 0, 0, // 蓝色通道的新值
-        0,      0,      0,      1, 0, // Alpha通道保持不变
-      ]),
-      child: child,
-    ),
-  );
+  Widget build(BuildContext context) => disable
+      ? AbsorbPointer(
+          child: ColorFiltered(
+            colorFilter: const .matrix(<double>[
+              0.2126, 0.7152, 0.0722, 0, 0, // 红色通道的新值
+              0.2126, 0.7152, 0.0722, 0, 0, // 绿色通道的新值
+              0.2126, 0.7152, 0.0722, 0, 0, // 蓝色通道的新值
+              0, 0, 0, 1, 0, // Alpha通道保持不变
+            ]),
+            child: child,
+          ),
+        )
+      : child;
 }
